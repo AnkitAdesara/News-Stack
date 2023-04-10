330ad75535f6bf121903f0e6939548d680f0d920
AnkitAdesara <123370467+AnkitAdesara@users.noreply.github.com>
2023-04-10 23:14:09 +0530

    Add files via upload

---
+++ fashion.js
@@ -0,0 +1,19 @@
+let f = fetch(" https://inshorts.deta.dev/news?category=fashion")
+f.then((value) => {
+    return value.json()
+}).then((valuee) => {
+    // console.log(valuee)
+    ihtml=""
+    for(items in valuee.data){
+        ihtml += `<div class="box">
+            <img src="${valuee.data[items].imageUrl}">
+            <div class="internalbox">
+                <p>Title : ${valuee.data[items].title}</p>
+                <p>Author : ${valuee.data[items].author}</p>
+                <p>Date : ${valuee.data[items].date} , Time: ${valuee.data[items].time}</p>
+                <p>Content : ${valuee.data[items].content}</p>
+            </div>
+        </div>`
+    }
+    document.getElementById("content").innerHTML = ihtml
+})
